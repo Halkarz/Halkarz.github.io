4996998eb141ea14eacbea6828fe0430f71dad7b
Halkarz <161700720+Halkarz@users.noreply.github.com>
2024-02-29 13:21:10 +0300

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -23,6 +23,186 @@ const halkarz= [
     market: "BIST", // Pazar
     image: "https://halkarz.com/wp-content/uploads/2023/09/MOGAN.jpg"
   },
+  {
+    id: 3,
+    name: "ARTMS",
+    description: "Artemis Halı A.Ş.",
+    listingDate: "28/02/2024",
+    priceRange: "80-90",
+    shares: "500000",
+    broker: "DEF Securities",
+    stockCode: "ARTMS",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/ARTMS.jpg"
+  },
+  {
+    id: 4,
+    name: "OBAMS",
+    description: "Oba Makarnacılık San. ve Tic. A.Ş.",
+    listingDate: "23/02/2024",
+    priceRange: "50-60",
+    shares: "800000",
+    broker: "GHI Securities",
+    stockCode: "OBAMS",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/OBAMS.jpg"
+  },
+  {
+    id: 5,
+    name: "ALVES",
+    description: "Alves Kablo San. ve Tic. A.Ş.",
+    listingDate: "23/02/2024",
+    priceRange: "70-80",
+    shares: "600000",
+    broker: "JKL Securities",
+    stockCode: "ALVES",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/ALVES.jpg"
+  },
+  {
+    id: 6,
+    name: "LMKDC",
+    description: "Limak Doğu Anadolu Çimento San. ve Tic. A.Ş.",
+    listingDate: "16/02/2024",
+    priceRange: "40-50",
+    shares: "1200000",
+    broker: "MNO Securities",
+    stockCode: "LMKDC",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/LMKDC.jpg"
+  },
+  {
+    id: 7,
+    name: "BORSK",
+    description: "Bor Şeker A.Ş.",
+    listingDate: "09/02/2024",
+    priceRange: "30-40",
+    shares: "1500000",
+    broker: "PQR Securities",
+    stockCode: "BORSK",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/BORSK.jpg"
+  },
+  {
+    id: 8,
+    name: "PATEK",
+    description: "Pasifik Donanım ve Yazılım Bilgi Teknolojileri A.Ş.",
+    listingDate: "07/02/2024",
+    priceRange: "60-70",
+    shares: "700000",
+    broker: "STU Securities",
+    stockCode: "PATEK",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/PATEK.jpg"
+  },
+  {
+    id: 9,
+    name: "AVPGY",
+    description: "Avrupakent Gayrimenkul Yatırım Ortaklığı A.Ş.",
+    listingDate: "15/12/2023",
+    priceRange: "25-35",
+    shares: "2000000",
+    broker: "VWX Securities",
+    stockCode: "AVPGY",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/AVPGY.jpg"
+  },
+  {
+    id: 10,
+    name: "MEGMT",
+    description: "Mega Metal San. ve Tic. A.Ş.",
+    listingDate: "08/12/2023",
+    priceRange: "45-55",
+    shares: "900000",
+    broker: "YZA Securities",
+    stockCode: "MEGMT",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/MEGMT.jpg"
+  },
+  {
+    id: 11,
+    name: "KBORU",
+    description: "Kuzey Boru A.Ş.",
+    listingDate: "08/12/2023",
+    priceRange: "50-60",
+    shares: "800000",
+    broker: "BCD Securities",
+    stockCode: "KBORU",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/KBORU.jpg"
+  },
+  {
+    id: 12,
+    name: "SURGY",
+    description: "Sur Tatil Evleri Gayrimenkul Yatırım Ortaklığı A.Ş.",
+    listingDate: "08/12/2023",
+    priceRange: "40-50",
+    shares: "1000000",
+    broker: "EFG Securities",
+    stockCode: "SURGY",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/SURGY.jpg"
+  },
+  {
+    id: 13,
+    name: "CATES",
+    description: "Çates Elektrik Üretim A.Ş.",
+    listingDate: "01/12/2023",
+    priceRange: "35-45",
+    shares: "1500000",
+    broker: "HIJ Securities",
+    stockCode: "CATES",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/CATES.jpg"
+  },
+  {
+    id: 14,
+    name: "SKYMD",
+    description: "Şeker Yatırım Menkul Değerler A.Ş.",
+    listingDate: "01/12/2023",
+    priceRange: "20-30",
+    shares: "2000000",
+    broker: "KLM Securities",
+    stockCode: "SKYMD",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/SKYMD.jpg"
+  },
+  {
+    id: 15,
+    name: "BEGYO",
+    description: "Batı Ege Gayrimenkul Yatırım Ortaklığı A.Ş.",
+    listingDate: "01/12/2023",
+    priceRange: "25-35",
+    shares: "1500000",
+    broker: "NOP Securities",
+    stockCode: "BEGYO",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/BEGYO.jpg"
+  },
+  {
+    id: 16,
+    name: "AGROT",
+    description: "Agrotech Yüksek Teknoloji ve Yatırım A.Ş.",
+    listingDate: "17/11/2023",
+    priceRange: "40-50",
+    shares: "1000000",
+    broker: "QRS Securities",
+    stockCode: "AGROT",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/AGROT.jpg"
+  },
+  {
+    id: 17,
+    name: "EKOS",
+    description: "Ekos Teknoloji ve Elektrik A.Ş.",
+    listingDate: "17/11/2023",
+    priceRange: "60-70",
+    shares: "700000",
+    broker: "TUV Securities",
+    stockCode: "EKOS",
+    market: "BIST",
+    image: "https://halkarz.com/wp-content/uploads/2023/09/EKOS.jpg"
+  }
 ];
 
 export default halkarz;
